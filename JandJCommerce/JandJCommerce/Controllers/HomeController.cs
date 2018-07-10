ebedae56fb8c0fba322b3b8aa8d7c012d55b4e10
Jimmy Chang <jimmyfchang94@gmail.com>
2018-07-10 09:47:14 -0700

    finishes dependency injection (user story 4)

--- JandJCommerce/JandJCommerce/Controllers/HomeController.cs
+++ JandJCommerce/JandJCommerce/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JandJCommerce.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,13 @@ namespace JandJCommerce.Controllers
 {
     public class HomeController : Controller
     {
+        private IInventory _inventory;
+
+        public HomeController(IInventory inventory)
+        {
+            _inventory = inventory;
+        }
+
         public IActionResult Index ()
         {
             return View();
