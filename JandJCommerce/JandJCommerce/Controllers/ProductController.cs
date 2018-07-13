6638a8da329a919ae04ea1b6578b9de08ab7c1f8
Judah Hunger <judah.hunger@gmail.com>
2018-07-13 15:39:54 -0700

    added pictures to use in assets and some seed data

--- JandJCommerce/JandJCommerce/Controllers/ProductController.cs
+++ JandJCommerce/JandJCommerce/Controllers/ProductController.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JandJCommerce.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JandJCommerce.Controllers
 {
     public class ProductController : Controller
     {
+        private IInventory _inventory;
+
+        public ProductController(IInventory inventory)
+        {
+            _inventory = inventory;
+        }
+
         public IActionResult Index()
         {
             return View();
