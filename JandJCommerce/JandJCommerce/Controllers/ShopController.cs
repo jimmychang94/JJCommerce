a4cddf8c31cb73258da398f4f030a1e66107dc4b
Jimmy Chang <jimmyfchang94@gmail.com>
2018-07-15 23:02:49 -0700

    adds shop controller and edits html for index page

---
+++ JandJCommerce/JandJCommerce/Controllers/ShopController.cs
@@ -0,0 +1,33 @@
+﻿using JandJCommerce.Models;
+using JandJCommerce.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JandJCommerce.Controllers
+{
+    public class ShopController : Controller
+    {
+        private IInventory _inventory;
+
+        public ShopController(IInventory inventory)
+        {
+            _inventory = inventory;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> allProducts = _inventory.GetProducts().Result;
+            return View(allProducts);
+        }
+
+        [HttpGet(Name = "Details")]
+        public async Task<IActionResult> Details(int id)
+        {
+            Product product = await _inventory.GetProductById(id);
+            return View(product);
+        }
+    }
+}
